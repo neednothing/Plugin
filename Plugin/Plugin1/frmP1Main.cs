1c1f048cdc469b41eb15aabfcdddb5933917a5f0
neednothing <97853807+neednothing@users.noreply.github.com>
2022-02-08 22:14:26 +0300

    Add files via upload

---
+++ Plugin/Plugin1/frmP1Main.cs
@@ -0,0 +1,30 @@
+﻿#region Using
+
+using System;
+using System.Windows.Forms;
+using PlugIn;
+
+#endregion
+
+namespace Plugin1
+{
+    public partial class FrmP1Main : Form
+    {
+        private readonly IPlugin _plug;
+
+        public FrmP1Main( IPlugin plug )
+        {
+            this.InitializeComponent();
+            this._plug = plug;
+        }
+
+        private void FrmP1Main_Load( object sender, EventArgs e )
+        {
+            this.tbInfo.AppendText( this._plug.Author + "\r\n" );
+            this.tbInfo.AppendText( this._plug.DisplayPluginName + "\r\n" );
+            this.tbInfo.AppendText( this._plug.PluginDescription + "\r\n" );
+            this.tbInfo.AppendText( this._plug.PluginName + "\r\n" );
+            this.tbInfo.AppendText( this._plug.Version + "\r\n" );
+        }
+    }
+}
---
+++ Plugin/Plugin2/PlugIn.cs
@@ -0,0 +1,59 @@
+﻿#region Using
+
+using PlugIn;
+
+#endregion
+
+namespace Plugin2
+{
+    public class PlugIn : IPlugin
+    {
+        private string _PluginName = "Plugin2";
+        private string _DisplayPluginName = "Второй плагин";
+        private string _PluginDescription = "Описание второго плагина";
+        private string _Author = "Dev";
+        private int _Version = 100;
+        private IPluginHost _Host;
+
+        public void Show()
+        {
+            FrmP2Main frm = new FrmP2Main( this );
+            frm.ShowDialog();
+        }
+
+        public IPluginHost Host
+        {
+            get { return this._Host; }
+            set
+            {
+                this._Host = value;
+                this._Host.Register( this );
+            }
+        }
+
+        public string PluginName
+        {
+            get { return this._PluginName; }
+        }
+
+        public string DisplayPluginName
+        {
+            get { return this._DisplayPluginName; }
+        }
+
+        public string PluginDescription
+        {
+            get { return this._PluginDescription; }
+        }
+
+        public string Author
+        {
+            get { return this._Author; }
+        }
+
+        public int Version
+        {
+            get { return this._Version; }
+        }
+    }
+}
